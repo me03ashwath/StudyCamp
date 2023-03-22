2f85ee9de86ff891715dc9b969e0b7a333b9d3f0
Ashwath S <99113851+me03ashwath@users.noreply.github.com>
2023-03-22 22:02:44 +0530

    commit 2

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
+from .forms import RoomForm
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    search = request.GET.get('search') if request.GET.get('search') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=search) |
+        Q(name__icontains=search) |
+        Q(description__icontains=search) |
+        Q(host__username__icontains=search)
+        )
+    room_count = rooms.count()
+
+    topics = Topic.objects.all()
+
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -12,3 +24,36 @@ def room(request, pk):
     context = {'room':room}
 
     return render(request, 'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'obj':room}
+    return render(request, 'base/delete.html', context)
+    
