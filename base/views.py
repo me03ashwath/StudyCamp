60842198c2f78a093fc88607bc16e50cd550efaf
Ashwath S <99113851+me03ashwath@users.noreply.github.com>
2023-03-25 20:51:06 +0530

    added comments and recent-activity

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 def loginPage(request):
@@ -63,17 +63,30 @@ def home(request):
         Q(description__icontains=search) |
         Q(host__username__icontains=search)
         )
+    
     room_count = rooms.count()
-
     topics = Topic.objects.all()
+    activities = Message.objects.filter(Q(room__topic__name__icontains=search)).order_by('-created')
 
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    context = {'rooms':rooms, 'topics':topics, 
+               'room_count':room_count, 'activities':activities}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    messages = Message.objects.filter(room=room).order_by('created')
+    participants = room.participants.all()
 
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id) # type: ignore
+
+    context = {'room':room, 'room_messages':messages, 'participants':participants}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='/login')
@@ -117,5 +130,19 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     context = {'obj':room}
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='/login')
+def deleteMessage(request, pk, roomId):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk=roomId)
+
+    context = {'obj':message}
     return render(request, 'base/delete.html', context)
     
